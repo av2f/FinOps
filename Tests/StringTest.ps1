4d0dbba6a2a64d4fbbc39b18eb57a8cf38a2a6f1
Parmentier <fparmentier@free.fr>
2024-04-25 13:18:05 +0200

    Start on working on V3 of Get-AzAllTags

--- Tests/StringTest.ps1
+++ Tests/StringTest.ps1
@@ -1,3 +1,28 @@
-$a = " AIP Code "
-$b = $a.replace(" ","")
-Write-Host "a=$a et b=$b"
+$string = "Environment,AIPCode,AIPName,AIPOwner,AIPCriticality,Customer,DepartmentName,Owner,Supported,SLA,ServiceWindows,IsItemizable"
+# $string2 = "Environment","AIPCode","AIPName","AIPOwner","AIPCriticality","Customer","DepartmentName","Owner","Supported","SLA","ServiceWindows","IsItemizable"
+$a="Environment"
+$c= @()
+$c = $string.split(",")
+Write-Host $c.GetType()
+$b="environment"
+
+foreach ($c1 in $c) {
+  write-Host $c1
+}
+
+$d = @()
+$d += "Toto"
+$d += "titi"
+$e = "{" + ($d -join ',') + "}"
+
+$myString = "dddd"
+Write-host $myString.Length
+
+
+Write-Host $e
+# Write-Host $string2
+
+if ($a -cin $c) {
+  Write-Host "$a found"
+}
+else { Write-Host "$a not found"}
