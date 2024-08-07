ab375e6eead412db13d202228ddcad9101ad8311
Parmentier <fparmentier@free.fr>
2024-08-07 17:29:52 +0200

    Set-AzBillingSynthesis.py: add function to uniformize tags

--- Set-AzBillingSynthesis/Set-AzBillingSynthesis.py
+++ Set-AzBillingSynthesis/Set-AzBillingSynthesis.py
@@ -13,6 +13,7 @@ import json
 import os
 import re
 import datetime
+import time
 
 # Declare constant
 JSON_FILE = 'Set-AzBillingSynthesis.json'
@@ -158,9 +159,41 @@ def get_finops_tags(tags, list_tags):
         if tag_value:
           dic_tags[finops_tag] = tag_value.group(1).strip()
   return dic_tags
+
+def uniformize_tags(df):
+  """
+    Retrieve the most recent value of the column Tags for a resource and
+    assigns it to all other lines matching this resource
+    Input: df the dataframe
+    Output: uniformize tags of resources
+  """
+  # Retrieve list of resources
+  u_resources = df['ResourceName'].unique()
+  # if at least 1 resource
+  if len(u_resources) > 0:
+    # Extract resources with date and tags
+    resources = df[['Date', 'ResourceName', 'Tags']]
+    c = 0
+    for u_resource in u_resources:
+      s_resource = resources[resources['ResourceName'] == u_resource]
+      # if at least 2 rows
+      if len(s_resource) > 2:
+        # sort by date descending
+        s_resource = s_resource.sort_values(by='Date', ascending=False)
+        # Keep the 1st row
+        s_resource = s_resource.iloc[0]
+        # Assigns value of tags to other rows with same resource
+        df.loc[df['ResourceName'] == u_resource, 'Tags'] = s_resource['Tags']
+        c += 1
+        if c % 1000 == 0:
+          msg = f'{c} resources processes...'
+          print(msg)
 #
 # ---------------------- main program
 def main():
+
+  # Calculate time execution
+  start = time.time()
 
   # === sera mis en parametres
   csv_source_file = 'Detail_Enrollment_88991105_202401_en.csv'
@@ -248,10 +281,17 @@ def main():
 
   # Convert Date in date format
   df['Date'] = pd.to_datetime(df['Date'], format = '%m/%d/%Y')
+
+  # Uniformize tags
+  uniformize_tags(df)
   
   # Write result file
   df.to_csv(target_file, sep=',', index=False)
   
   print(target_file)
+  
+  # Calulate time execution
+  end = time.time()
+  print(end - start)
 
 main()
