72b5fbb1da0e0143e146ea239bc1d491c41ee192
Parmentier <fparmentier@free.fr>
2024-08-09 09:39:53 +0200

    Set-AzBillingSynthesis: add description of functions

--- Set-AzBillingSynthesis/Set-AzBillingSynthesis.py
+++ Set-AzBillingSynthesis/Set-AzBillingSynthesis.py
@@ -4,7 +4,9 @@
   Version : 1.0
   Creation Date : 08/05/2024
 
-  ****** PUT DESCRIPTION *******
+  This script creates a synthesis file from Azure Detailed and usage charges file
+  if the file is not from the current month, data are grouping by resources
+  if the file is from the currenth month, data are not grouping
 """
 
 import pandas as pd
@@ -20,7 +22,7 @@ JSON_FILE = 'Set-AzBillingSynthesis.json'
 
 def read_json(file):
   """
-    Retrieve parameters in the Json parameters file
+    Retrieves parameters in the Json parameters file
     Input:
       - file: Json parameters file
     Output: 
@@ -31,21 +33,27 @@ def read_json(file):
 
 def create_target_directory(directory):
   """
-    Check if directory exists.
-    If not the case, create it
+    Checks if directory exists.
+    If not the case, creates it
+    Input:
+      - directory: directory to be created
+    Output:
+      - result:
+        + True if the directory exists or the creation is OK
+        + False if there is an error during the creation of the directory
   """
+  result = True
   if not os.path.exists(directory):
     try:
       os.mkdir(directory)
     except OSError as error:
       print(error)
-      return False
-  else:
-    return True
+      result = False
+  return result
   
 def calculate_duration(start, end):
   """
-    Calculate and format the duration of execution of script
+    Calculates and format the duration of execution of script
     Input:
       - start: script start time in seconds
       - end: script end time
@@ -56,13 +64,22 @@ def calculate_duration(start, end):
   return time.strftime('%Hh:%Mm:%Ss', time_elapse)
 
 def load_file(source_file, separator, csv_encoding):
+  """
+    Loads the csv source_file in dataframe filering columns
+    Input:
+      - source_file: the csv file to be loaded
+      - separator: separator used in the csv file (defined in the Json file)
+      - csv_encoding: encoding of the file (defined in the Json file)
+    Output: 
+      - pandas dataframe
+  """
   dtype_dict = {
       'BillingAccountId': 'str', 'BillingAccountName': 'str', 'BillingPeriodEndDate': 'str', 'BillingProfileId': 'str', 'BillingProfileName': 'str',
       'AccountOwnerId': 'str', 'AccountName': 'str', 'SubscriptionName': 'str', 'Date': 'str', 'MeterCategory': 'str', 'MeterSubCategory': 'str',
       'MeterName': 'str', 'Cost': 'float64', 'UnitPrice': 'float64', 'BillingCurrency': 'str', 'ResourceLocation': 'str', 'ConsumedService': 'str',
       'ResourceName': 'str', 'AdditionalInfo': 'str', 'Tags': 'str', 'CostCenter': 'str', 'ResourceGroup': 'str', 'ReservationName': 'str',
       'ProductOrderName': 'str', 'Term': 'str', 'ChargeType': 'str', 'PayGPrice': 'float64', 'PricingModel': 'str'
-    }
+  }
 
   return pd.read_csv(source_file, dtype=dtype_dict, sep=separator, encoding=csv_encoding, usecols=[
       'BillingAccountId', 'BillingAccountName', 'BillingPeriodEndDate', 'BillingProfileId', 'BillingProfileName',
@@ -70,9 +87,17 @@ def load_file(source_file, separator, csv_encoding):
       'MeterName', 'Cost', 'UnitPrice',	'BillingCurrency', 'ResourceLocation', 'ConsumedService',
       'ResourceName', 'AdditionalInfo', 'Tags', 'CostCenter', 'ResourceGroup', 'ReservationName',
       'ProductOrderName', 'Term', 'ChargeType', 'PayGPrice', 'PricingModel'
-    ])
+  ])
 
 def synthesis_file(df, finops_tags):
+  """
+    Groups the dataframe by resources, calculating the total cost per row
+    Input:
+      - df: dataframe to group
+      - finops_tags: List of FinOps tags keys
+    Output: 
+      - pandas dataframe
+  """
   tags = finops_tags.split(',')
   return df.groupby([
   'BillingAccountId', 'BillingPeriodEndDate', 'BillingProfileId', 'AccountOwnerId',
@@ -82,6 +107,14 @@ def synthesis_file(df, finops_tags):
   'ProductOrderName', 'Term', 'ChargeType', 'PayGPrice', 'PricingModel'] + tags, as_index=False, dropna=False).agg(Total_Cost = ('Cost', 'sum'))
 
 def get_billing_account(csvfile, df):
+  """
+    Retrieves billing account columns and writes them in csvfile if there are not yet present.
+    Input:
+      - csvfile: csv file to write result
+      - df: dataframe
+    Output: 
+      - csvfile updated
+  """
   # Put description
   billing_csv = [] 
   ba = df[['BillingAccountId', 'BillingAccountName']]
@@ -110,6 +143,14 @@ def get_billing_account(csvfile, df):
           writer.writerow(billing_id)
 
 def get_billing_profile(csvfile, df):
+  """
+    Retrieves billing profile columns and writes them in csvfile if there are not yet present.
+    Input:
+      - csvfile: csv file to write result
+      - df: dataframe
+    Output: 
+      - csvfile updated
+  """
   # Put description
   billing_csv = [] 
   bp = df[['BillingProfileId', 'BillingProfileName', 'BillingCurrency']]
@@ -138,7 +179,14 @@ def get_billing_profile(csvfile, df):
           writer.writerow(billing_id)
 
 def get_sku(info, criteria):
-  # Put Description
+  """
+    extracts from AdditionalInfo the SKU for Virtual Machines
+    Input:
+      - info: content of the AdditionalInfo column
+      - criteria: field to search (defined in Json file)
+    Output: 
+      - new_info: column AdditionalInfo with only the SKU if exists
+  """
   new_info = ''
   str_info = str(info)
   if criteria in str_info:
@@ -148,6 +196,13 @@ def get_sku(info, criteria):
   return new_info
 
 def get_reservation_type(product):
+  """
+    extracts from ProductName the type of reservation
+    Input:
+      - product: content of the ProductName column
+    Output: 
+      - new_product: column ProductName with only the type of reservation if exists
+  """
   new_product = ''
   str_product = str(product)
   if len(str_product) > 3:
@@ -158,12 +213,28 @@ def get_reservation_type(product):
   return new_product
 
 def set_finops_tags(df, finops_tags):
+  """
+    Creates new columns in dataframe corresponding to the FinOps Tags
+    Input:
+      - df: the dataframe
+      - finops_tags: list of FinOps tags keys (defined in the Json file)
+    Output: 
+      - df: dataframe with new column
+  """
   tags = finops_tags.split(',')
   for tag in tags:
     df[tag] = ''
   return df
 
 def get_finops_tags(tags, list_tags):
+  """
+    extracts FinOps tags from the Tags column and updates the column with only FinOps tags
+    Input:
+      - tags: content of the column Tags
+      - list_tags: list of FinOps tags keys (defined in the Json file)
+    Output: 
+      - str_dic_tags: new content of the column Tags with only FinOps tags
+  """
   dic_tags = {}
   str_tags = str(tags)
   if len(str_tags) > 3:
@@ -180,6 +251,14 @@ def get_finops_tags(tags, list_tags):
   return str_dic_tags
 
 def set_finops_tag(tags, key_tag):
+  """
+    extracts FinOps tags from the Tags column and updates the columns matching with the key value
+    Input:
+      - tags: content of the column Tags
+      - key_tag: FinOps tag key to search
+    Output: 
+      - tag_value: new content of the column corresponding to the FinOps Key value
+  """
   tag_value = ''
   if len(tags) > 0:
     if key_tag in tags:
@@ -192,8 +271,7 @@ def set_finops_tag(tags, key_tag):
 # ---------------------- main program
 def main():
 
-  # Calculate time execution
-  start = time.time()
+  start = time.time() # start of script execution
 
   # === sera mis en parametres
   csv_source_file = 'Detail_Enrollment_88991105_202405_en.csv'
@@ -300,8 +378,9 @@ def main():
   
   print(target_file)
   
+  end = time.time() # end of script execution
+  
   # Calulate time execution
-  end = time.time()
   duration = calculate_duration(start, end)
   print ('Script executed in ' + duration)
 
